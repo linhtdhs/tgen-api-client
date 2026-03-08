4bbd7444358b88090f245f884964b0bf178ca11d
Linh Tran <39045407+linhtdhs@users.noreply.github.com>
2026-03-08 19:02:44 +0700

    chore: Split files into Models/Services folder (#3)

--- Services/HttpService.cs
+++ Services/HttpService.cs
@@ -3,19 +3,10 @@ using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using tgenapiclient.Models;
 
 namespace tgenapiclient.Services;
 
-public class HttpResponseData
-{
-    public int StatusCode { get; set; }
-    public string ReasonPhrase { get; set; } = string.Empty;
-    public long ElapsedMilliseconds { get; set; }
-    public int SizeBytes { get; set; }
-    public string Body { get; set; } = string.Empty;
-    public string Headers { get; set; } = string.Empty;
-    public bool IsSuccess { get; set; }
-}
 
 public class HttpService
 {
